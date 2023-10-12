dd73ed2afd50db4accdf93070b0c88862e164e93
Jake Bruemmer <jakebruemmer@gmail.com>
2023-10-12 11:14:14 -0600

    Got tests passing.

---
+++ index.js
@@ -0,0 +1,31 @@
+let add = function (a, b) {
+  return a + b;
+}
+
+let subtract = function (a, b) {
+  return a - b;
+}
+
+let multiply = function (a, b) {
+  return a * b;
+}
+
+let divide = function (a, b) {
+  return a / b;
+}
+
+let increment = function (n) {
+  return n + 1;
+}
+
+let decrement = function (n) {
+  return n - 1;
+}
+
+let makeInt = function (n) {
+  return parseInt(n, 10);
+}
+
+let preserveDecimal = function (n) {
+  return parseFloat(n);
+}
